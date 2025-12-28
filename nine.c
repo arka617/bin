5babbe34748fbdc9b81744fbe7d0bddfa18a8a39
Arka Ballove <arkaballove270@gmail.com>
2025-12-28 21:43:25 +0600

    Rename variable 'mis' to 'mi' in nine.c

--- nine.c
+++ nine.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    int x,y,z,a,mis;
+    int x,y,z,a,mi;
 scanf("%d %d %d %d",&x,&y,&z,&a);
-mis=x*y*z*a;
-printf("%d",mis);
+mi=x*y*z*a;
+printf("%d",mi);
 return 0;
 }
