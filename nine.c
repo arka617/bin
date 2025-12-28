0139f0e9dae0495e89714c52b2921e45f7be226c
Arka Ballove <arkaballove270@gmail.com>
2025-12-28 21:23:38 +0600

    Modify multiplication to include four integers

--- nine.c
+++ nine.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    int x,y,z,mul;
-scanf("%d %d",&x,&y,&z);
-mul=x*y*z;
+    int x,y,z,a,mul;
+scanf("%d %d %d %d",&x,&y,&z,&a);
+mul=x*y*z*a;
 printf("%d",mul);
 return 0;
 }
