40509925ac6cad47b42cd27437cf01b359c8786f
Arka Ballove <arkaballove270@gmail.com>
2025-12-28 21:28:01 +0600

    Rename variable 'mul' to 'm' in nine.c

--- nine.c
+++ nine.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    int x,y,z,a,mul;
+    int x,y,z,a,m;
 scanf("%d %d %d %d",&x,&y,&z,&a);
-mul=x*y*z*a;
-printf("%d",mul);
+m=x*y*z*a;
+printf("%d",m);
 return 0;
 }
